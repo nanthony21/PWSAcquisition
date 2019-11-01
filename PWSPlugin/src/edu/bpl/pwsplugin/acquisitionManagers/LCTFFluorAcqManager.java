3e1aa9e9cbb360998cb52170770350bd39316024
nickAnthonyNU <nicholas.anthony@northwestern.edu>
2019-11-01 15:19:24 -0500

    rather than setting a BF filter block name. Just make sure to return back to the original block when we're done

--- PWSPlugin/src/edu/bpl/pwsplugin/acquisitionManagers/LCTFFluorAcqManager.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/acquisitionManagers/LCTFFluorAcqManager.java
@@ -6,7 +6,6 @@
 package edu.bpl.pwsplugin.acquisitionManagers;
 
 import edu.bpl.pwsplugin.Globals;
-import edu.bpl.pwsplugin.fileSavers.ImSaverRaw;
 import edu.bpl.pwsplugin.fileSavers.MMSaver;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
@@ -14,7 +13,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.LinkedBlockingQueue;
 import org.json.JSONObject;
-import org.micromanager.Studio;
 import org.micromanager.data.Coords;
 import org.micromanager.data.Image;
 import org.micromanager.data.Pipeline;
@@ -24,36 +22,44 @@ import org.micromanager.internal.utils.ReportingUtils;
  *
  * @author backman05
  */
-public class LCTFFluorAcqManager implements AcquisitionManager{
+public class LCTFFluorAcqManager extends FluorAcqManager{
     double exposure_; //The camera exposure in milliseconds.
     String filtLabel_; //The name of the spectral filter device
     int wavelength_; //The wavelength to acquire images at
-    String flFilterBlock_; // The name of the fluorescence filter changer device.
-    String bfFilterBlock_;
+    String flFilterBlock_; // The name of the fluorescence filter block config setting.
     boolean autoFilter_;
     
-    public LCTFFluorAcqManager(){
-    }
-    
-    public void setFluorescenceSettings(boolean autoFilter, String bfFilter, String flFilter, double exposure, int emissionWV, String tunableFilterLabel) {
+    public void setFluorescenceSettings(boolean autoFilter, String flFilter, double exposure, int emissionWV, String tunableFilterLabel) {
         autoFilter_ = autoFilter;
         filtLabel_ = tunableFilterLabel;
         flFilterBlock_ = flFilter;
-        bfFilterBlock_ = bfFilter;
         exposure_ = exposure;
         wavelength_ = emissionWV;
     }
     
     @Override
     public void acquireImages(String savePath, int cellNum, LinkedBlockingQueue imagequeue, JSONObject metadata) {
+        String fullSavePath;
+        String initialFilter = "";
         try {
-            String fullSavePath = this.getSavePath(savePath, cellNum); //This also checks if the file already exists, throws error if it does.
+            fullSavePath = this.getSavePath(savePath, cellNum); //This also checks if the file already exists, throws error if it does.
+        } catch (Exception e) {
+            ReportingUtils.showMessage("Fluoresence save path already exists. Cancelling.");
+            return;
+        }
+        try{
             if (autoFilter_) {
+                initialFilter = Globals.core().getCurrentConfig("Filter");
                 Globals.core().setConfig("Filter", flFilterBlock_);
                 Globals.core().waitForConfig("Filter", flFilterBlock_); // Wait for the device to be ready.
             } else {
-                ReportingUtils.showMessage("Set the correct fluorescence filter and click `OK`."); //TODO make sure this actually blocks.
+                ReportingUtils.showMessage("Set the correct fluorescence filter and click `OK`.");
             }
+        } catch (Exception e) {
+            ReportingUtils.showMessage("Failed to set fluoresence filter. Cancelling.");
+            return;
+        }
+        try {
             Globals.core().setProperty(filtLabel_, "Wavelength", wavelength_);
             Globals.core().setExposure(exposure_);
             Globals.core().clearCircularBuffer();
@@ -76,8 +82,8 @@ public class LCTFFluorAcqManager implements AcquisitionManager{
         } finally {
             if (autoFilter_) {
                 try {
-                    Globals.core().setConfig("Filter", bfFilterBlock_);
-                    Globals.core().waitForConfig("Filter", bfFilterBlock_); // Wait for the device to be ready.
+                    Globals.core().setConfig("Filter", initialFilter);
+                    Globals.core().waitForConfig("Filter", initialFilter); // Wait for the device to be ready.
                 } catch (Exception e){
                     ReportingUtils.showError(e);
                 }
@@ -85,24 +91,5 @@ public class LCTFFluorAcqManager implements AcquisitionManager{
                 ReportingUtils.showMessage("Return to the PWS filter block and click `OK`.");
             }
         }
-    }
-    
-    @Override
-    public String getSavePath(String savePath, int cellNum) throws FileAlreadyExistsException {
-        Path path = Paths.get(savePath).resolve("Cell" + String.valueOf(cellNum)).resolve("Fluorescence");
-        if (Files.isDirectory(path)){
-            throw new FileAlreadyExistsException("Cell " + cellNum + " fluorescence already exists.");
-        } 
-        return path.toString();
-    }
-    
-    @Override
-    public String getFilePrefix() {
-        return "fluor";
-    }
-    
-    @Override
-    public int getExpectedFrames() {
-        return 1;
     }
 }
