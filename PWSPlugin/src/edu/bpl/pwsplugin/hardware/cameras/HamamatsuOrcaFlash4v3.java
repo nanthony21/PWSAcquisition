010dc13a793193cb53d543640150a122b904e97b
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-02-03 17:01:17 -0600

    add some functionality to orcaflash

--- PWSPlugin/src/edu/bpl/pwsplugin/hardware/cameras/HamamatsuOrcaFlash4v3.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/hardware/cameras/HamamatsuOrcaFlash4v3.java
@@ -5,6 +5,9 @@
  */
 package edu.bpl.pwsplugin.hardware.cameras;
 
+import edu.bpl.pwsplugin.Globals;
+import java.util.List;
+
 /**
  *
  * @author N2-LiveCell
@@ -21,8 +24,20 @@ public class HamamatsuOrcaFlash4v3 extends Camera{
     public boolean supportsTriggerOutput() { return true; }
     
     @Override
-    public void configureTriggerOutput(boolean enable); //Turn transmission of TTL pulses on or off.
+    public void configureTriggerOutput(boolean enable) {
+        if (enable) {
+            Globals.core().setProperty(devName, "OUTPUT TRIGGER SOURCE[0]", "READOUT END");
+            Globals.core().setProperty(devName, "OUTPUT TRIGGER POLARITY[0]", "POSITIVE");
+            Globals.core().setProperty(devName, "OUTPUT TRIGGER KIND[0]", "PROGRAMMABLE");
+            Globals.core().setProperty(devName, "OUTPUT TRIGGER PERIOD[0]", 0.001); //The default is shorter than this and it is often missed by other devices.
+        } else {
+            Globals.core().setProperty(devName, "OUTPUT TRIGGER KIND[0]", "LOW");
+        }
+    }
     
     @Override
     public String getName(); //Get the device name used in Micro-Manager.
+    
+    @Override
+    public List<String> valideate();
 }
