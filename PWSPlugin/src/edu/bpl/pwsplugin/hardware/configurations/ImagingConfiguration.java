feacc66ff06386f7c168e4bfb655abbe7681620c
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-02-18 12:29:49 -0600

    add a first imaging config

--- PWSPlugin/src/edu/bpl/pwsplugin/hardware/configurations/ImagingConfiguration.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/hardware/configurations/ImagingConfiguration.java
@@ -16,4 +16,39 @@ public abstract class ImagingConfiguration {
     public abstract boolean hasTunableFilter();
     public abstract Camera camera();
     public abstract TunableFilter tunableFilter();
+    
+    public static ImagingConfiguration getInstance(Types type) {
+        if (type == Types.LCTFWithHam) {
+            return new LCTFWithHam();
+        } else {
+            return null; //This shouldn't ever happen.
+        }
+    }
+    
+    public enum Types {
+        LCTFWithHam;
+    }
+}
+
+class LCTFWithHam extends ImagingConfiguration {
+    Camera _cam;
+    TunableFilter _filt;
+    
+    public LCTFWithHam() {
+        _cam = Camera.getInstance(Camera.Types.HAMAMATSUORCA4V3);
+        _filt = TunableFilter.getInstance(TunableFilter.Types.VARISPECLCTF);
+    }
+    
+    @Override
+    public boolean hasTunableFilter() { return true; }
+    
+    @Override
+    public Camera camera() {
+        return _cam;
+    }
+    
+    @Override
+    public TunableFilter tunableFilter() {
+        return _filt;
+    }
 }
