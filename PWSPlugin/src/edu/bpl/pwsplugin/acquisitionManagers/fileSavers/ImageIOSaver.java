fc6b8fc93c2e0dc123fa96b1841e92c2aa08818b
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2020-06-20 20:09:13 -0500

    got ImageIO saver mostly working. metadata issue.

--- PWSPlugin/src/edu/bpl/pwsplugin/acquisitionManagers/fileSavers/ImageIOSaver.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/acquisitionManagers/fileSavers/ImageIOSaver.java
@@ -6,41 +6,31 @@
 package edu.bpl.pwsplugin.acquisitionManagers.fileSavers;
 
 import com.google.gson.JsonObject;
-import java.util.concurrent.LinkedBlockingQueue;
+import edu.bpl.pwsplugin.Globals;
 import java.nio.file.Paths;
 import org.micromanager.data.Image;
 import org.micromanager.internal.utils.ReportingUtils;
-import org.micromanager.data.Metadata;
 import javax.imageio.ImageWriter;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.IIOImage;
 import javax.imageio.metadata.IIOMetadata;
-import javax.imageio.metadata.IIOMetadataNode;
-import java.io.File;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import javax.imageio.stream.ImageOutputStream;
-import javax.imageio.ImageTypeSpecifier;
-//import com.twelvemonkeys.imageio.metadata.tiff.TIFF
-
-/**
- *
- * @author nick
- */
-import edu.bpl.pwsplugin.Globals;
 import edu.bpl.pwsplugin.metadata.MetadataBase;
+import ij.ImagePlus;
+import ij.io.FileInfo;
+import ij.io.FileSaver;
+import ij.plugin.ContrastEnhancer;
+import ij.process.ImageConverter;
 import edu.bpl.pwsplugin.utils.GsonUtils;
-import io.scif.media.imageio.plugins.tiff.BaselineTIFFTagSet;
-import io.scif.media.imageio.plugins.tiff.TIFFDirectory;
-import io.scif.media.imageio.plugins.tiff.TIFFField;
-import io.scif.media.imageio.plugins.tiff.TIFFTag;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import mmcorej.org.json.JSONException;
-
+import java.io.File;
+import java.io.FileOutputStream;
 public class ImageIOSaver extends SaverExecutor {
     private String savePath;
     private String fileName;
@@ -55,13 +45,15 @@ public class ImageIOSaver extends SaverExecutor {
     }
     
     @Override
-    public Void call() throws Exception {
-        ImageWriter writer = ImageIO.getImageWritersBySuffix("tif").next();        
+    public Void call() throws Exception { //This was tested using the TwelveMonkeys imageIO plugin for TIFF. In theory it should work for any ImageIO tiff plugin.
+        ImageWriter writer = ImageIO.getImageWritersBySuffix("tif").next(); //This will throw an error if a Tiff plugin can't be found. The default java version for Micro-Manager is version 8 which doesn't have  a plugin by default. One solution is just to make sure to use java 9 or higher.   
         ImageWriteParam param = configureWriter(writer);
         
         long startTime = System.currentTimeMillis();
         String fullFileName = String.format("%s.tif", this.fileName);
-        try (ImageOutputStream outStream = ImageIO.createImageOutputStream(Paths.get(this.savePath, fullFileName).toFile())) {
+        File directory = new File(this.savePath);
+        if (!directory.exists()) { directory.mkdirs(); }
+        try (ImageOutputStream outStream = ImageIO.createImageOutputStream(new FileOutputStream(Paths.get(this.savePath, fullFileName).toFile()))) {
             writer.setOutput(outStream);
             writer.prepareWriteSequence(null); //null means we will use the default streamMetadata.
             try {
@@ -77,7 +69,7 @@ public class ImageIOSaver extends SaverExecutor {
                     writer.writeToSequence(image, param);
                     
                     if (i == this.expectedFrames/2) { //Note: due to integer division this still works on i==0 for expectedFrames==1.
-                        saveThumbnail(image); //Save the thumbnail image from halfway through the sequence.
+                        saveThumbnail(mmImg, this.savePath, this.fileName); //Save the thumbnail image from halfway through the sequence.
                     }
                 }
                 //Now that we've recieved all the images check for the metadata and save it.
@@ -88,14 +80,7 @@ public class ImageIOSaver extends SaverExecutor {
                 JsonObject mdJson = md.toJson();
                 writeMetadata(this.savePath, this.fileName, mdJson); // saves metadata to a text file.
                 IIOMetadata iomd = writer.getDefaultStreamMetadata(param);
-                //IIOMetadataNode node = new IIOMetadataNode("PWSPluginMetadata");
-                //node.setNodeValue(GsonUtils.getGson().toJson(mdJson));
-                //iomd.mergeTree(iomd.getNativeMetadataFormatName(), node);
-                TIFFDirectory dir = TIFFDirectory.createFromMetadata(iomd);
-                TIFFTag tag = BaselineTIFFTagSet.getInstance().getTag(BaselineTIFFTagSet.TAG_IMAGE_DESCRIPTION);
-                TIFFField field = new TIFFField(tag, TIFFTag.TIFF_ASCII, 1, GsonUtils.getGson().toJson(mdJson));
-                dir.addTIFFField(field);
-                writer.replaceStreamMetadata(dir.getAsMetadata());//Also try to save it to the tiff file.
+                //Twelve monkeys does not support writing metadata after the fact, just use the json text file.
             } finally { // We don't need to catch any exceptions we can just have them get thrown.
                 writer.endWriteSequence();
                 writer.dispose();
@@ -118,10 +103,20 @@ public class ImageIOSaver extends SaverExecutor {
         return new IIOImage(bim ,null, null); // We can set metadata later, or maybe we don't need to, no need for thumbnails.
     }
     
-    private void saveThumbnail(IIOImage img) throws IOException {
-        try (ImageOutputStream ostream = ImageIO.createImageOutputStream(Paths.get(this.savePath, "image_bd.tif").toFile())) {
-            ImageIO.write(img.getRenderedImage(), "TIFF", ostream);
-        } 
+    private void saveThumbnail(Image im, String savePath, String filePrefix) throws IOException{
+        ImagePlus imPlus = new ImagePlus(filePrefix, Globals.mm().data().ij().createProcessor(im));
+        ContrastEnhancer contrast = new ContrastEnhancer();
+        contrast.stretchHistogram(imPlus,0.01); //I think this will saturate 0.01% of the image. or maybe its 1% idk. 
+        ImageConverter converter = new ImageConverter(imPlus);
+        converter.setDoScaling(true);
+        converter.convertToGray8();
+        FileInfo info = new FileInfo();
+        imPlus.setFileInfo(info);
+        FileSaver saver = new FileSaver(imPlus);
+        boolean success = saver.saveAsTiff(Paths.get(savePath).resolve("image_bd.tif").toString());
+        if (!success) {
+            throw new IOException("Image BD failed to save");
+        }
     }
     
     private void writeMetadata(String savePath, String filePrefix, JsonObject md) throws IOException {
@@ -144,37 +139,5 @@ class ImageIOHelper {
         r.setPixels(0,0,width,height,newarr);
         im.setData(r);
         return im;
-    }
-    
-    static void createTIFFFieldNode(final IIOMetadataNode parentIFDNode, int tag, short type, Object value) {
-        IIOMetadataNode fieldNode = new IIOMetadataNode("TIFFField");
-
-        fieldNode.setAttribute("number", String.valueOf(tag));
-        parentIFDNode.appendChild(fieldNode);
-
-        switch (type) {
-            case TIFF.TYPE_ASCII:
-                createTIFFFieldContainerNode(fieldNode, "Ascii", value);
-                break;
-            case TIFF.TYPE_BYTE:
-                createTIFFFieldContainerNode(fieldNode, "Byte", value);
-                break;
-            case TIFF.TYPE_SHORT:
-                createTIFFFieldContainerNode(fieldNode, "Short", value);
-                break;
-            case TIFF.TYPE_RATIONAL:
-                createTIFFFieldContainerNode(fieldNode, "Rational", value);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported type: " + type);
-        }
-    }
-
-    static void createTIFFFieldContainerNode(final IIOMetadataNode fieldNode, final String type, final Object value) {
-        IIOMetadataNode containerNode = new IIOMetadataNode("TIFF" + type + "s");
-        fieldNode.appendChild(containerNode);
-        IIOMetadataNode valueNode = new IIOMetadataNode("TIFF" + type);
-        valueNode.setAttribute("value", String.valueOf(value));
-        containerNode.appendChild(valueNode);
     }
 }
