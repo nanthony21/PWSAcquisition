409124b6945943337887fd190a44b605fd994996
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-09-02 14:38:18 -0500

    Don't force off PFS when starting a sequence.

--- PWSPlugin/src/edu/bpl/pwsplugin/acquisitionSequencer/steps/RootStep.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/acquisitionSequencer/steps/RootStep.java
@@ -50,7 +50,6 @@ public class RootStep extends ContainerStep<SequencerSettings.RootStepSettings> 
                 status.setCellNum(0);
                 status.setSavePath(settings.directory);
                 TranslationStage1d zstage = Globals.getHardwareConfiguration().getActiveConfiguration().zStage();
-                if (zstage.hasAutoFocus()) { zstage.setAutoFocusEnabled(false); } //Make sure to start with PFS turned off. A focus lock step needs to be used to enable focus again.
                 RootStep.this.saveToJson(Paths.get(settings.directory, "sequence.pwsseq").toString()); //Save the sequence to file for retrospect.
                 status = subStepFunc.apply(status);
                 //Experiment is now finished
