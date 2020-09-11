0466b1cfa3f7b3f3097788dd2033534cd047adc1
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2020-09-11 09:59:04 -0500

    TODO

--- PWSPlugin/src/edu/bpl/pwsplugin/PWSPlugin.java
+++ PWSPlugin/src/edu/bpl/pwsplugin/PWSPlugin.java
@@ -25,6 +25,7 @@ TODO
 ZStage autofocus routine bumps the sample? It seems like it shouldn't though. Maybe I'm wrong.
 test software autofocus
 test focus lock in realistic situation, ethanol cells and weird dishes.
+save/load config to file
 
 2:
 Make nikon recalibrate when objective changes. (fix config group event). Make objective a `Device`?
