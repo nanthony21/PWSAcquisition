543bc9998ed1d474d5d91e62f452a88da278269c
Nick Anthony <nicholas.anthony@northwestern.edu>
2020-09-30 12:23:18 -0500

    comment

--- src/main/java/edu/bpl/pwsplugin/acquisitionSequencer/UI/tree/Transferables.java
+++ src/main/java/edu/bpl/pwsplugin/acquisitionSequencer/UI/tree/Transferables.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.bpl.pwsplugin.acquisitionSequencer.UI.tree;
 
 import java.awt.datatransfer.DataFlavor;
@@ -16,6 +11,7 @@ import java.util.List;
  */
 public class Transferables {
     public static class NodesTransferable implements Transferable {
+        /* This `transferable` defines how a treenode's data is transmitted during a drag/drop operation.*/
         List<CopyableMutableTreeNode> nodes;
         DataFlavor[] flavors = new DataFlavor[1];
 
