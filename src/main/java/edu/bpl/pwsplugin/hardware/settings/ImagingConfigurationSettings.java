484e84cd4f6e7303a57893dce44f549befe4d3df
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-01-28 15:27:20 -0600

    Add license headers

--- src/main/java/edu/bpl/pwsplugin/hardware/settings/ImagingConfigurationSettings.java
+++ src/main/java/edu/bpl/pwsplugin/hardware/settings/ImagingConfigurationSettings.java
@@ -1,8 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+///////////////////////////////////////////////////////////////////////////////
+//PROJECT:       PWS Plugin
+//
+//-----------------------------------------------------------------------------
+//
+// AUTHOR:       Nick Anthony, 2021
+//
+// COPYRIGHT:    Northwestern University, 2021
+//
+// LICENSE:      This file is distributed under the BSD license.
+//               License text is included with the source distribution.
+//
+//               This file is distributed in the hope that it will be useful,
+//               but WITHOUT ANY WARRANTY; without even the implied warranty
+//               of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//               IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//               CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//               INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
 package edu.bpl.pwsplugin.hardware.settings;
 
 import edu.bpl.pwsplugin.hardware.configurations.ImagingConfiguration;
